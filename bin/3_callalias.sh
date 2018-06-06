70440a42f53ca9e811606ab24a14fd1abc07aefd
Ziqi Deng <ziqi.deng@embl.de>
2018-06-06 12:56:01 +0100

    removed wrong files

--- bin/3_callalias.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-alias chim='/Applications/Chimera.app/Contents/MacOS/chimera'
-
-cd pdb_file/
-ls *.pdb > instanceList.txt 
-
-chim --nostatus --script "auto_findhbond_script_template_operation.py"
